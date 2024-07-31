055c7bdbd73d28f58b4429e3575f69f6354fa467
Digiland.tw <digiland5@gmail.com>
2024-08-01 05:13:01 +0800

    Update TouchScreen.h
    support ESP32S3 UNO

--- TouchScreen.h
+++ TouchScreen.h
@@ -9,10 +9,14 @@
 
 // ESP32 specific 
 #define ESP32_WIFI_TOUCH // uncomment to use parallel MCU Friend LCD touchscreen with ESP32 UNO Wifi
-#ifdef ESP32 
-#define ADC_MAX 4095  // maximum value for ESP32 ADC (default 11db, 12 bits)
-#define aXM 35  // analog input pin connected to LCD_RS 
-#define aYP 39  // analog input pin connected to LCD_WR
+#ifdef CONFIG_IDF_TARGET_ESP32S3  // for ESP32S3 Uno
+	#define ADC_MAX 4095  // maximum value for ESP32 ADC (default 11db, 12 bits)
+	#define aXM 7	// analog input pin connected to LCD_RS 
+	#define aYP 1	// analog input pin connected to LCD_WR	
+#elif defined(ESP32)	
+	#define ADC_MAX 4095  // maximum value for ESP32 ADC (default 11db, 12 bits)
+	#define aXM 35  // analog input pin connected to LCD_RS 
+	#define aYP 39  // analog input pin connected to LCD_WR
 #else
 #define ADC_MAX 1023  // Arduino
 #endif 
